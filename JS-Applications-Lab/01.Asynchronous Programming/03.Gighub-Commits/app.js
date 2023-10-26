c2d6830008cf08bd350e722a6f363945dc1dda3d
Todor Mladenov <toshko958@gmail.com>
2023-10-26 21:53:16 +0200

    Problem 3 Github Commit from lab - done

--- JS-Applications-Lab/01.Asynchronous Programming/03.Gighub-Commits/app.js
+++ JS-Applications-Lab/01.Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,36 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let usernameElement = document.getElementById('username');
+    let repoElement = document.getElementById('repo');
+    let commitsElement = document.getElementById('commits');
+
+    let username = usernameElement.value;
+    let repo = repoElement.value;
+
+    let url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            return res.json();
+        })
+        .then(commits => {
+            commitsElement.innerHTML = '';
+            commits.forEach(commit => {
+                let name = commit.commit.author.name;
+                let message = commit.commit.message;
+
+                let liElement = document.createElement('li');
+                liElement.textContent = `${name}: ${message}`;
+                commitsElement.appendChild(liElement);
+            });
+        })
+        .catch((error) => {
+            commitsElement.innerHTML = '';
+            let liElement = document.createElement('li');
+            liElement.textContent = `Error: ${error.message} (Not Found)`;
+            commitsElement.appendChild(liElement);
+        })
 }
