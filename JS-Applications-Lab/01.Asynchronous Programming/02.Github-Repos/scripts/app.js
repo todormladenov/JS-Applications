a5362ba41b62b9798c0c41f434bc1f3474c7ceac
Todor Mladenov <toshko958@gmail.com>
2023-10-26 16:42:00 +0200

    Second problem form the lab: Github Repos. Done

--- JS-Applications-Lab/01.Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ JS-Applications-Lab/01.Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,30 @@
 function loadRepos() {
-	console.log("TODO...");
+	let baseUrl = 'https://api.github.com/users';
+	let reposElement = document.getElementById('repos');
+	let usernameElement = document.getElementById('username');
+
+	fetch(`${baseUrl}/${usernameElement.value}/repos`)
+		.then(res => res.json())
+		.then(repos => {
+			reposElement.innerHTML = '';
+
+			repos.forEach((repo) => {
+				let liElement = document.createElement('li');
+
+				let aRepoElement = document.createElement('a');
+				aRepoElement.textContent = repo.full_name;
+				aRepoElement.href = repo.html_url;
+
+				liElement.appendChild(aRepoElement)
+				reposElement.appendChild(liElement);
+			});
+		})
+		.catch((err) => {
+			let liElement = document.createElement('li');
+			liElement.textContent = `Error: 404 (Not Found)`;
+
+			reposElement.appendChild(liElement);
+
+		});
+
 }
