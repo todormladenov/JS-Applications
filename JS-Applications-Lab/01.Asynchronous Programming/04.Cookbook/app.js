5111ae99354d270135e1c4fc9d3a35afc884ac50
Todor Mladenov <toshko958@gmail.com>
2023-10-27 12:30:41 +0200

    Cookbook problem form lab done

---
+++ JS-Applications-Lab/01.Asynchronous Programming/04.Cookbook/app.js
@@ -0,0 +1,88 @@
+async function getRecipes() {
+    let response = await fetch('http://localhost:3030/jsonstore/cookbook/recipes');
+    let recipes = await response.json();
+
+    return Object.values(recipes);
+}
+
+async function getRecipeById(id) {
+    let response = await fetch(`http://localhost:3030/jsonstore/cookbook/details/${id}`);
+    let recipeInfo = await response.json();
+
+    return recipeInfo;
+}
+
+window.addEventListener('load', async () => {
+    let main = document.querySelector('main');
+    main.innerHTML = '';
+    let recipes = await getRecipes();
+
+    recipes.forEach(recipe => {
+        let recipeElement = createRecipe(recipe);
+        let id = recipe._id;
+
+        recipeElement.addEventListener('click', async () => {
+            let recipeDetails = await addDetails(id);
+            recipeElement.replaceWith(recipeDetails);
+        });
+    });
+});
+
+function createRecipe(recipe) {
+    let main = document.querySelector('main');
+
+    let articleElement = elementCreator('article', null, null, 'preview', main);
+    let divTitleElement = elementCreator('div', null, null, 'title', articleElement);
+    let divSmallElement = elementCreator('div', null, null, 'small', articleElement);
+
+    elementCreator('h2', recipe.name, null, null, divTitleElement);
+    elementCreator('img', null, recipe.img, null, divSmallElement);
+
+    return articleElement;
+}
+
+async function addDetails(id) {
+    let recipeInfo = await getRecipeById(id);
+
+    let articleElement = elementCreator('article', null, null, null, null);
+    elementCreator('h2', recipeInfo.name, null, null, articleElement);
+
+    let divBandElement = elementCreator('div', null, null, 'band', articleElement);
+    let divThumbElement = elementCreator('div', null, null, 'thumb', divBandElement);
+    elementCreator('img', null, recipeInfo.img, null, divThumbElement);
+
+    let divIngredientsElement = elementCreator('div', null, null, 'ingredients', divBandElement);
+    elementCreator('h3', 'Ingredients:', null, null, divIngredientsElement);
+
+    let ulIngredientsElement = elementCreator('ul', null, null, null, divIngredientsElement);
+    recipeInfo.ingredients.forEach(ing => elementCreator('li', ing, null, null, ulIngredientsElement));
+
+    let divDescriptionElement = elementCreator('div', null, null, 'description', articleElement);
+    elementCreator('h3', 'Preparation:', null, null, divDescriptionElement);
+
+    recipeInfo.steps.forEach(step => elementCreator('p', step, null, null, divDescriptionElement))
+
+    return articleElement;
+}
+
+function elementCreator(type, text, src, classStyle, parent) {
+    let el = document.createElement(type);
+
+    if (text) {
+        el.textContent = text;
+    }
+
+    if (src) {
+        el.src = src
+    }
+
+    if (classStyle) {
+        el.setAttribute('class', classStyle);
+    }
+
+    if (parent) {
+        parent.appendChild(el);
+    }
+
+    return el;
+}
