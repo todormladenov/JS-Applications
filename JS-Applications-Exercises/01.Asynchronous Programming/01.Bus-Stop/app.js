f8b218279cdc06df40bbf15f725169d42b223bfa
Todor Mladenov <toshko958@gmail.com>
2023-10-28 19:31:27 +0200

    First problem from the exercise done

---
+++ JS-Applications-Exercises/01.Asynchronous Programming/01.Bus-Stop/app.js
@@ -0,0 +1,31 @@
+function getInfo() {
+    let baseUrl = `http://localhost:3030/jsonstore/bus/businfo/`;
+    let stopIdElement = document.getElementById('stopId');
+    let stopNameElement = document.getElementById('stopName');
+    let busesElement = document.getElementById('buses');
+
+    fetch(baseUrl + stopIdElement.value)
+        .then(response => {
+            busesElement.replaceChildren();
+
+            if (response.status != 200 || stopIdElement.value == '') {
+                throw new Error('Error');
+            }
+
+            return response.json();
+        })
+        .then(busesInfo => {
+            stopNameElement.textContent = busesInfo.name;
+
+            Object.entries(busesInfo.buses).forEach(stopInfo => {
+                let liElement = document.createElement('li');
+
+                liElement.textContent = `Bus ${stopInfo[0]} arrives in ${stopInfo[1]} minute`;
+                busesElement.appendChild(liElement);
+            })
+        })
+        .catch(err => {
+            stopNameElement.textContent = err.message;
+        })
+
+}
