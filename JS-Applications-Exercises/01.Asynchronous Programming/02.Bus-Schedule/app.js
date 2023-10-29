df6bab3dbcfa34a37a7c8ef508e5aa5e522cf425
Todor Mladenov <toshko958@gmail.com>
2023-10-29 09:19:32 +0100

    Completed problem number 2.

--- JS-Applications-Exercises/01.Asynchronous Programming/02.Bus-Schedule/app.js
+++ JS-Applications-Exercises/01.Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
+    let infoElement = document.querySelector('.info');
+    let departBtnElement = document.getElementById('depart');
+    let arriveBtnElement = document.getElementById('arrive');
+
+    let stop = {
+        name: '',
+        next: 'depot'
+    }
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch('http://localhost:3030/jsonstore/bus/schedule/' + stop.next)
+        .then(response => response.json())
+        .then(schedule => {
+            stop.name = schedule.name;
+            stop.next = schedule.next;
+            infoElement.textContent = `Next Stop ${stop.name}`;
+        })
+        arriveBtnElement.disabled = false;
+        departBtnElement.disabled = true;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoElement.textContent = `Arriving at ${stop.name}`;
+        departBtnElement.disabled = false;
+        arriveBtnElement.disabled = true;
     }
 
     return {
