0e18606c33a4e3859ba7c3e672333755065423af
Todor Mladenov <toshko958@gmail.com>
2023-11-02 15:45:05 +0100

    Completed the second problem form the exercise

--- JS-Applications-Exercises/02.Remote Data and Authentication/02.Phonebook/app.js
+++ JS-Applications-Exercises/02.Remote Data and Authentication/02.Phonebook/app.js
@@ -1,5 +1,74 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+    let url = 'http://localhost:3030/jsonstore/phonebook';
 
+    let phonebookElement = document.getElementById('phonebook');
+    let loadBtnElement = document.getElementById('btnLoad');
+    let createBtnElement = document.getElementById('btnCreate');
+    let personElement = document.getElementById('person');
+    let phoneElement = document.getElementById('phone');
+
+    createBtnElement.addEventListener('click', addInfo);
+    loadBtnElement.addEventListener('click', loadPhoneInfo);
+
+    async function loadPhoneInfo() {
+        let response = await fetch(url);
+        let phonebook = await response.json();
+        phonebookElement.innerHTML = '';
+
+        Object.values(phonebook).forEach(el => {
+
+            let { person, phone, _id } = el;
+            let liInfoElement = elementCreator('li', `${person}:${phone}`, phonebookElement);
+            let deleteBtnElement = elementCreator('button', 'Delete', liInfoElement);
+
+            deleteBtnElement.addEventListener('click', async () => {
+                liInfoElement.remove();
+
+                let deleteUrl = `http://localhost:3030/jsonstore/phonebook/${_id}`;
+                let response = await fetch(deleteUrl, {
+                    method: 'DELETE',
+                });
+            });
+        });
+    }
+
+    async function addInfo() {
+        if (personElement.value == '' && phoneElement.value == '') {
+            return;
+        }
+
+        let info = {
+            person: personElement.value,
+            phone: phoneElement.value
+        }
+
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(info)
+        });
+        let data = await response.json();
+
+        personElement.value = '';
+        phoneElement.value = '';
+    }
+
+    function elementCreator(type, text, parent) {
+        let el = document.createElement(type);
+
+        if (text) {
+            el.textContent = text
+        }
+
+        if (parent) {
+            parent.appendChild(el);
+        }
+
+        return el
+    }
+
+
+}
 attachEvents();
