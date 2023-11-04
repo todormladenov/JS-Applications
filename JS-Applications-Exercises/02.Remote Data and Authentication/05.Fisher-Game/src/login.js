8aa1f64438efb927d6ec4e6087c37e5c1c4a4e0c
Todor Mladenov <toshko958@gmail.com>
2023-11-04 16:00:09 +0100

    Added the login function

--- JS-Applications-Exercises/02.Remote Data and Authentication/05.Fisher-Game/src/login.js
+++ JS-Applications-Exercises/02.Remote Data and Authentication/05.Fisher-Game/src/login.js
@@ -1,1 +1,40 @@
-console.log('TODO:// Implement Login functionality');
+window.addEventListener('load', () => {
+    let formElement = document.querySelector('form');
+    let notificationElement = formElement.querySelector('.notification');
+    formElement.addEventListener('submit', login);
+
+    async function login(e) {
+        e.preventDefault();
+        
+        let url = 'http://localhost:3030/users/login';
+        let formData = getFormData();
+
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(formData)
+        });
+
+        let user = await response.json();
+
+        if (!response.ok) {
+            notificationElement.textContent = user.message;
+            return;
+        }
+
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+
+    function getFormData() {
+        let formData = new FormData(formElement);
+        notificationElement.textContent = '';
+
+        let email = formData.get('email');
+        let password = formData.get('password');
+
+        return {
+            email,
+            password,
+        }
+    }
+});
