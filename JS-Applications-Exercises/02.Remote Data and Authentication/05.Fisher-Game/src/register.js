8009b7185d409619f405311731c90c618b96a63f
Todor Mladenov <toshko958@gmail.com>
2023-11-04 14:27:26 +0100

    Implemented registration functionality

--- JS-Applications-Exercises/02.Remote Data and Authentication/05.Fisher-Game/src/register.js
+++ JS-Applications-Exercises/02.Remote Data and Authentication/05.Fisher-Game/src/register.js
@@ -1,1 +1,47 @@
-console.log('TODO:// Implement Register functionality');
+window.addEventListener('load', () => {
+    let formElement = document.querySelector('form');
+    let notificationElement = formElement.querySelector('.notification');
+    formElement.addEventListener('submit', register);
+
+    async function register(e) {
+        e.preventDefault();
+    
+        let formData = getFormData();   
+        let url = 'http://localhost:3030/users/register';
+
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(formData)
+        })
+        let user = await response.json();
+
+        if (!response.ok) {
+            notificationElement.textContent = user.message;
+            return;
+        }
+
+        localStorage.setItem(user, JSON.stringify(user));
+        notificationElement.textContent = `Hello ${user.email} you have registered successful!`;
+
+    }
+
+    function getFormData() {
+        let formData = new FormData(formElement);
+        notificationElement.textContent = '';
+
+        let email = formData.get('email');
+        let password = formData.get('password');
+        let rePass = formData.get('rePass');
+
+        if (password != rePass) {
+            notificationElement.textContent = 'Password must match repeatPassword.'
+            return;
+        }
+
+        return {
+            email,
+            password,
+        }
+    }
+});
