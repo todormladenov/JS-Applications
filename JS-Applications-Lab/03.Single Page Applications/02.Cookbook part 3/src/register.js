6bd421d4a77f71a8e13055efcea583a28742dd2e
Todor Mladenov <toshko958@gmail.com>
2023-11-06 12:49:01 +0100

    Created register functionality

--- JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/register.js
+++ JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/register.js
@@ -1,5 +1,50 @@
-const registerSectionElement = document.querySelector('.register');
+import { auth } from "./authentication.js";
 
-export function renderRegister(){
+const registerSectionElement = document.querySelector('.register');
+const registerFormElement = registerSectionElement.querySelector('form');
+registerFormElement.addEventListener('submit', register);
+
+export function renderRegister() {
     registerSectionElement.style.display = 'block';
 }
+
+async function register(e) {
+    e.preventDefault();
+
+    let formData = new FormData(registerFormElement);
+
+    let email = formData.get('email');
+    let password = formData.get('password');
+    let rePass = formData.get('rePass');
+
+    if (password != rePass) {
+        alert('Password must match repeat');
+        return;
+    }
+
+    try {
+        let response = await fetch('http://localhost:3030/users/register', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+        if (response.status != 200) {
+            let error = await response.json();
+            throw new Error(error.message);
+        }
+
+        let user = await response.json();
+        localStorage.setItem('email', user.email);
+        localStorage.setItem('accessToken', user.accessToken);
+        localStorage.setItem('_id', user._id);
+
+        alert('Successfully registered.');
+        registerFormElement.reset();
+        auth();
+
+    } catch (error) {
+        alert(`Error: ${error.message}`);
+    }
+
+}
+
