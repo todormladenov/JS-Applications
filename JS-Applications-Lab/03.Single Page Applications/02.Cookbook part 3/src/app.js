fc74485d547e9551c5c4b6133ab1b0c618538093
Todor Mladenov <toshko958@gmail.com>
2023-11-06 11:25:13 +0100

    Created login functionality

--- JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/app.js
+++ JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/app.js
@@ -3,14 +3,27 @@ import { router } from "./router.js";
 const guestElement = document.getElementById('guest');
 const userElement = document.getElementById('user');
 const navigationElement = document.querySelector('.navigation');
-guestElement.style.display = 'inline';
-userElement.style.display = 'inline';
+
+const accessToken = localStorage.getItem('accessToken');
+
+if (accessToken) {
+    guestElement.style.display = 'none';
+    userElement.style.display = 'inline';
+} else {
+    guestElement.style.display = 'inline';
+    userElement.style.display = 'none';
+}
+
 
 navigationElement.addEventListener('click', (e) => {
     e.preventDefault();
-    
+
     if (e.target.tagName == 'A') {
         let url = new URL(e.target.href);
+
+        document.querySelector('.active').classList.remove('active');
+        e.target.classList.add('active');
+
         router(url.pathname);
     }
 });
--- JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/login.js
+++ JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/login.js
@@ -1,5 +1,31 @@
-let loginSectionElement = document.querySelector('.login');
+const loginSectionElement = document.querySelector('.login');
+const loginFormElement = loginSectionElement.querySelector('form');
 
-export function renderLogin(){
+loginFormElement.addEventListener('submit', login);
+
+export function renderLogin() {
     loginSectionElement.style.display = 'block';
+}
+
+async function login(e) {
+    e.preventDefault();
+
+    let formData = new FormData(loginFormElement);
+
+    let email = formData.get('email');
+    let password = formData.get('password');
+
+    let response = await fetch('http://localhost:3030/users/login', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    })
+    let user = await response.json();
+    
+    localStorage.setItem('email', user.email);
+    localStorage.setItem('username', user.username);
+    localStorage.setItem('accessToken', user.accessToken);
+    localStorage.setItem('_id', user._id);
+
+    alert('Successfully logged in.')
 }
