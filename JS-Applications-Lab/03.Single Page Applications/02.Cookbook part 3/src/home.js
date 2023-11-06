029468aef7402844b8943cb13c6f6304b772d5e3
Todor Mladenov <toshko958@gmail.com>
2023-11-06 14:29:20 +0100

    Home generates all recipes preview

--- JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/home.js
+++ JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/home.js
@@ -1,5 +1,28 @@
+const url = 'http://localhost:3030/data/recipes'
 const homeSection = document.querySelector('.home');
 
-export function renderHome(){
+export async function renderHome() {
+    let response = await fetch(url);
+    let recipes = await response.json();
+    
+    homeSection.innerHTML = '';
+    recipes.forEach(recipe => {
+        showRecipes(recipe);
+    });
+
     homeSection.style.display = 'block';
+}
+
+function showRecipes(recipe) {
+    const recipeElement = document.createElement('article');
+    recipeElement.classList.add('preview');
+    recipeElement.innerHTML = ` 
+                <div class="title">
+                    <h2>${recipe.name}</h2>
+                </div>
+                <div class="small">
+                    <img src="${recipe.img}">
+                </div>`;
+                
+    homeSection.appendChild(recipeElement);
 }
