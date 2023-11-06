b566663053248d4930d4f4d79006f6bcb98f957b
Todor Mladenov <toshko958@gmail.com>
2023-11-06 11:38:56 +0100

    Added error handling

--- JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/login.js
+++ JS-Applications-Lab/03.Single Page Applications/02.Cookbook part 3/src/login.js
@@ -1,3 +1,5 @@
+import { auth } from "./authentication.js";
+
 const loginSectionElement = document.querySelector('.login');
 const loginFormElement = loginSectionElement.querySelector('form');
 
@@ -11,21 +13,32 @@ async function login(e) {
     e.preventDefault();
 
     let formData = new FormData(loginFormElement);
-
     let email = formData.get('email');
     let password = formData.get('password');
 
-    let response = await fetch('http://localhost:3030/users/login', {
-        method: 'POST',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify({ email, password })
-    })
-    let user = await response.json();
-    
-    localStorage.setItem('email', user.email);
-    localStorage.setItem('username', user.username);
-    localStorage.setItem('accessToken', user.accessToken);
-    localStorage.setItem('_id', user._id);
+    try {
+        let response = await fetch('http://localhost:3030/users/login', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+        if (response.status != 200) {
+            let error = await response.json();
+            throw new Error(error)
+        }
 
-    alert('Successfully logged in.')
+        let user = await response.json();
+
+        localStorage.setItem('email', user.email);
+        localStorage.setItem('username', user.username);
+        localStorage.setItem('accessToken', user.accessToken);
+        localStorage.setItem('_id', user._id);
+
+        alert('Successfully logged in.');
+        loginFormElement.reset();
+        auth();
+
+    } catch (error) {
+        alert(`Error: ${error.message}`);
+    }
 }
