278f26ce9aa646e57011c5befdaf71ee7ad98eeb
Todor Mladenov <toshko958@gmail.com>
2023-11-07 14:24:32 +0100

    Update requests.js

--- JS-Applications-Exercises/03.Single Page Applications/01.Forum/src/requests.js
+++ JS-Applications-Exercises/03.Single Page Applications/01.Forum/src/requests.js
@@ -1,6 +1,7 @@
-const url = 'http://localhost:3030/jsonstore/collections/myboard/posts'
+const url = 'http://localhost:3030/jsonstore/collections/myboard/posts';
+const commentsURL = 'http://localhost:3030/jsonstore/collections/myboard/comments';
 
-export async function sendPost(postInfo){
+export async function sendPost(postInfo) {
     try {
         let response = await fetch(url, {
             method: 'POST',
@@ -19,7 +20,36 @@ export async function sendPost(postInfo){
     }
 }
 
-export async function getAllPosts(){
+export async function sendComment(data){
+    try {
+        let response = await fetch(commentsURL, {
+            method: 'POST',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(data)
+        })
+        if (response.status != 200) {
+            let error = await response.json();
+            throw new Error(error.message);
+        }
+    
+        let comment = await response.json();
+        return comment;
+    } catch (error) {
+        alert(`Error: ${error.message}`);
+    }
+}
+
+export async function getAllCommentsById(){
+    try {
+        let response = await fetch(commentsURL);
+        let comments = await response.json();
+        return Object.values(comments);
+    } catch (error) {
+        alert(`Error: ${error.message}`);
+    }
+}
+
+export async function getAllPosts() {
     try {
         let response = await fetch(url)
         if (response.status != 200) {
@@ -32,4 +62,20 @@ export async function getAllPosts(){
     } catch (error) {
         alert(`Error: ${error.message}`)
     }
+}
+
+export async function getPostById(id) {
+    try {
+        let response = await fetch(`${url}/${id}`);
+        let post = await response.json();
+        if (response.status != 200) {
+            let error = await response.json();
+            throw new Error(error.message)
+        }
+
+        return post;
+    } catch (error) {
+        alert(`Error: ${error.message}`);
+    }
+
 }
