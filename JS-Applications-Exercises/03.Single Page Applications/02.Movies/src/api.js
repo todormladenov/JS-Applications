9d0e55db95c08183134519789ed854ffe8a2d2ad
Todor Mladenov <toshko958@gmail.com>
2023-11-08 16:13:04 +0100

    Added register functionality

--- JS-Applications-Exercises/03.Single Page Applications/02.Movies/src/api.js
+++ JS-Applications-Exercises/03.Single Page Applications/02.Movies/src/api.js
@@ -2,7 +2,10 @@ import { get, post } from "./requests.js";
 
 const baseUrl = 'http://localhost:3030';
 const loginUrl = `${baseUrl}/users/login`;
+const registerUrl = `${baseUrl}/users/register`;
+const logoutUrl = `${baseUrl}/users/logout`;
 const getMoviesUrl = `${baseUrl}/data/movies`;
 
 export const loginRequest = (data) => post(loginUrl, data);
 export const getAllMovies = () => get(getMoviesUrl);
+export const registerRequest = (registerData) => post(registerUrl, registerData);
--- JS-Applications-Exercises/03.Single Page Applications/02.Movies/src/register.js
+++ JS-Applications-Exercises/03.Single Page Applications/02.Movies/src/register.js
@@ -1,6 +1,38 @@
+import { registerRequest } from "./api.js";
+import { showHome } from "./home.js";
+
 const containerElement = document.getElementById('container');
 const registerSectionElement = document.getElementById('form-sign-up');
+const registerFormElement = document.getElementById('register-form');
+registerFormElement.addEventListener('submit', register);
 
 export function showRegister(){
     containerElement.appendChild(registerSectionElement);
+}
+
+async function register(e){
+    e.preventDefault();
+
+    let formData = new FormData(registerFormElement);
+
+    let email = formData.get('email');
+    let password = formData.get('password');
+    let repeatPassword = formData.get('repeatPassword');
+
+    if (password != repeatPassword) {
+        alert('Password must match Repeat Password');
+    }
+
+    let registerData = {
+        email,
+        password
+    }
+
+    let user = await registerRequest(registerData);
+
+    if (user) {
+        localStorage.setItem('user', JSON.stringify(user));
+        registerFormElement.reset();
+        showHome();
+    }
 }
