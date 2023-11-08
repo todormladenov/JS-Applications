0ac4f59e8942c5add0de145cd7d72435b15b6a2b
Todor Mladenov <toshko958@gmail.com>
2023-11-08 15:42:25 +0100

    Update home.js to load all movies

--- JS-Applications-Exercises/03.Single Page Applications/02.Movies/src/api.js
+++ JS-Applications-Exercises/03.Single Page Applications/02.Movies/src/api.js
@@ -2,5 +2,7 @@ import { get, post } from "./requests.js";
 
 const baseUrl = 'http://localhost:3030';
 const loginUrl = `${baseUrl}/users/login`;
+const getMoviesUrl = `${baseUrl}/data/movies`;
 
 export const loginRequest = (data) => post(loginUrl, data);
+export const getAllMovies = () => get(getMoviesUrl);
--- JS-Applications-Exercises/03.Single Page Applications/02.Movies/src/home.js
+++ JS-Applications-Exercises/03.Single Page Applications/02.Movies/src/home.js
@@ -3,8 +3,36 @@ import { authenticator } from "./auth.js";
 
 const containerElement = document.getElementById('container');
 const homeSectionElement = document.getElementById('home-page');
+const movieListElement = document.getElementById('movies-list');
 
-export function showHome(){
+export function showHome() {
     authenticator();
     containerElement.appendChild(homeSectionElement);
+    generateAllMovies();
+}
+
+async function generateAllMovies() {
+    movieListElement.innerHTML = '';
+    let allMovies = await getAllMovies();
+    allMovies.forEach(movieData => createMoviePreview(movieData));
+}
+
+function createMoviePreview(movieData) {
+    let element = document.createElement('div');
+    element.className = 'card mb-4';
+
+    element.innerHTML = `
+    <img class="card-img-top" src=${movieData.img} 
+        alt="Card image cap" width="400">
+    <div class="card-body">
+            <h4>${movieData.title}</h4>
+    </div>
+    <div class="card-footer">
+            <a data-id=${movieData._id} href="/details/${movieData._id}">
+            <button type="button" class="btn btn-info">Details</button>
+            </a>    
+    </div>`;
+
+    movieListElement.appendChild(element);
+    return element;
 }
