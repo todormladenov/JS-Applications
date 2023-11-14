8296b013f25d184e92c181f48a43d469a1bc2be7
Todor Mladenov <toshko958@gmail.com>
2023-11-14 22:25:44 +0100

    Created create.js

---
+++ JS-Applications-Exercises/06.Routing/src/create.js
@@ -0,0 +1,72 @@
+import { render, html } from "../node_modules/lit-html/lit-html.js";
+import { createRequest } from "./requests.js";
+
+const container = document.querySelector('.container');
+
+const createTemplate = (createFurniture) => html`
+<div class="row space-top">
+            <div class="col-md-12">
+                <h1>Create New Furniture</h1>
+                <p>Please fill all fields.</p>
+            </div>
+        </div>
+        <form @submit=${createFurniture}>
+            <div class="row space-top">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-make">Make</label>
+                        <input class="form-control valid" id="new-make" type="text" name="make">
+                    </div>
+                    <div class="form-group has-success">
+                        <label class="form-control-label" for="new-model">Model</label>
+                        <input class="form-control is-valid" id="new-model" type="text" name="model">
+                    </div>
+                    <div class="form-group has-danger">
+                        <label class="form-control-label" for="new-year">Year</label>
+                        <input class="form-control is-invalid" id="new-year" type="number" name="year">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-description">Description</label>
+                        <input class="form-control" id="new-description" type="text" name="description">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-price">Price</label>
+                        <input class="form-control" id="new-price" type="number" name="price">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-image">Image</label>
+                        <input class="form-control" id="new-image" type="text" name="img">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-material">Material (optional)</label>
+                        <input class="form-control" id="new-material" type="text" name="material">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Create" />
+                </div>
+            </div>
+</form>`;
+
+export function showCreate(cxt) {
+    cxt.updateNav();
+    render(createTemplate(createFurniture), container);
+
+    async function createFurniture(e) {
+        e.preventDefault();
+        let form = e.currentTarget
+        let formData = new FormData(form);
+
+        let make = formData.get('make');
+        let model = formData.get('model');
+        let year = formData.get('year');
+        let description = formData.get('description');
+        let price = formData.get('price');
+        let img = formData.get('img');
+        let material = formData.get('material');
+
+        await createRequest({ make, model, year, description, price, img, material });
+        form.reset();
+        cxt.page.redirect('/');
+    }
+}
