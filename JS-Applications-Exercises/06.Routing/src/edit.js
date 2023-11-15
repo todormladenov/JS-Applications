a44952904f5019edb78c08095ab1883f9142e14f
Todor Mladenov <toshko958@gmail.com>
2023-11-15 12:57:07 +0100

    Update error handling

--- JS-Applications-Exercises/06.Routing/src/edit.js
+++ JS-Applications-Exercises/06.Routing/src/edit.js
@@ -2,7 +2,7 @@ import { render, html } from "../node_modules/lit-html/lit-html.js";
 import { createEditRequest, getFurnitureById } from "./requests.js";
 const container = document.querySelector('.container');
 
-const editFormTemplate = (itemData, editFurniture) => html`
+const editFormTemplate = (itemData, editFurniture, errorMsg, errors) => html`
 <div class="row space-top">
             <div class="col-md-12">
                 <h1>Edit Furniture</h1>
@@ -10,33 +10,34 @@ const editFormTemplate = (itemData, editFurniture) => html`
             </div>
         </div>
         <form @submit=${editFurniture}>
+        ${errorMsg ? html`<div class="form-group">${errorMsg}</div>` : null}
             <div class="row space-top">
                 <div class="col-md-4">
                     <div class="form-group">
                         <label class="form-control-label" for="new-make">Make</label>
-                        <input class="form-control" id="new-make" type="text" name="make" value="${itemData.make}">
+                        <input class=${'form-control' + (errors.make ? ' is-invalid' : '')} id="new-make" type="text" name="make" value="${itemData.make}">
                     </div>
                     <div class="form-group has-success">
                         <label class="form-control-label" for="new-model">Model</label>
-                        <input class="form-control is-valid" id="new-model" type="text" name="model" value="${itemData.model}">
+                        <input class=${'form-control' + (errors.model ? ' is-invalid' : '')} id="new-model" type="text" name="model" value="${itemData.model}">
                     </div>
                     <div class="form-group has-danger">
                         <label class="form-control-label" for="new-year">Year</label>
-                        <input class="form-control is-invalid" id="new-year" type="number" name="year" value="${itemData.year}">
+                        <input class=${'form-control' + (errors.year ? ' is-invalid' : '')} id="new-year" type="number" name="year" value="${itemData.year}">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-description">Description</label>
-                        <input class="form-control" id="new-description" type="text" name="description" value="${itemData.description}">
+                        <input class=${'form-control' + (errors.description ? ' is-invalid' : '')} id="new-description" type="text" name="description" value="${itemData.description}">
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="form-group">
                         <label class="form-control-label" for="new-price">Price</label>
-                        <input class="form-control" id="new-price" type="number" name="price" value="${itemData.price}">
+                        <input class=${'form-control' + (errors.price ? ' is-invalid' : '')} id="new-price" type="number" name="price" value="${itemData.price}">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-image">Image</label>
-                        <input class="form-control" id="new-image" type="text" name="img" value="${itemData.img}">
+                        <input class=${'form-control' + (errors.img ? ' is-invalid' : '')} id="new-image" type="text" name="img" value="${itemData.img}">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-material">Material (optional)</label>
@@ -49,23 +50,34 @@ const editFormTemplate = (itemData, editFurniture) => html`
 
 export async function showEdit(ctx) {
     let itemData = await getFurnitureById(ctx.params.id)
-    render(editFormTemplate(itemData, editFurniture), container);
+    render(editFormTemplate(itemData, editFurniture, null, {}), container);
 
     async function editFurniture(e) {
         e.preventDefault();
         let form = e.currentTarget
-        let formData = new FormData(form);
+        let formData = [...(new FormData(form)).entries()];
 
-        let make = formData.get('make');
-        let model = formData.get('model');
-        let year = formData.get('year');
-        let description = formData.get('description');
-        let price = formData.get('price');
-        let img = formData.get('img');
-        let material = formData.get('material');
+        const data = formData.reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
+        const missing = formData.filter(([k, v]) => k != 'material' && v.trim() == '');
 
-        await createEditRequest({ make, model, year, description, price, img, material }, ctx.params.id);
-        form.reset();
-        ctx.page.redirect('/');
+        try {
+            if (missing.length) {
+                const errors = missing.reduce((a, [k]) => Object.assign(a, { [k]: true }), {})
+                throw {
+                    error: new Error('Please fill all mandatory fields!'),
+                    errors
+                };
+            }
+            data.year = Number(data.year);
+            data.price = Number(data.price);
+
+            await createEditRequest(data, ctx.params.id);
+            form.reset();
+            ctx.page.redirect('/');
+        } catch (err) {
+            console.log(err);
+            const message = err.message || err.error.message;
+            render(editFormTemplate(itemData, editFurniture, message, err.errors), container);
+        }
     }
 }
