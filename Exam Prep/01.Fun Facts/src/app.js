ac48f8db945db6232249f5e328c851f32d31a59c
Todor Mladenov <toshko958@gmail.com>
2023-11-15 21:38:03 +0100

    Created app.js

---
+++ Exam Prep/01.Fun Facts/src/app.js
@@ -0,0 +1,45 @@
+import page from "../node_modules/page/page.mjs";
+import { createView } from "./create.js";
+import { dashboardView } from "./dashboard.js";
+import { detailsView } from "./details.js";
+import { editView } from "./edit.js";
+import { homeView } from "./home.js";
+import { loginView } from "./login.js";
+import { registerView } from "./register.js";
+
+page(decorateContext);
+page('/', homeView);
+page('/create', createView);
+page('/dashboard', dashboardView);
+page('/login', loginView);
+page('/register', registerView);
+page('/details/:id', detailsView)
+page('/edit/:id', editView)
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.updateNavBar = () => updateNavBar();
+
+    next();
+}
+
+function updateNavBar() {
+    const userView = document.querySelector('.user');
+    const guestView = document.querySelector('.guest');
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    if (user) {
+        userView.style.display = 'inline';
+        guestView.style.display = 'none';
+    } else {
+        userView.style.display = 'none';
+        guestView.style.display = 'inline';
+    }
+}
+
+document.getElementById('logout-btn').addEventListener('click', () => {
+    localStorage.clear();
+    updateNavBar();
+    page.redirect('/');
+});
