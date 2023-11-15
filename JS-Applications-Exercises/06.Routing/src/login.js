3cef18edd07c9383acb97d574bc80f2ffe3bbe95
Todor Mladenov <toshko958@gmail.com>
2023-11-15 12:57:11 +0100

    Update error handling

--- JS-Applications-Exercises/06.Routing/src/login.js
+++ JS-Applications-Exercises/06.Routing/src/login.js
@@ -3,7 +3,7 @@ import { loginRequest } from "./requests.js";
 
 const container = document.querySelector('.container');
 
-const loginTemplate = (login) => html`
+const loginTemplate = (login, errorMsg) => html`
 <div class="row space-top">
 <div class="col-md-12">
     <h1>Login User</h1>
@@ -13,13 +13,14 @@ const loginTemplate = (login) => html`
 <form @submit=${login}>
 <div class="row space-top">
     <div class="col-md-4">
+    ${errorMsg ? html`<div class="form-group">${errorMsg}</div>` : null}
         <div class="form-group">
             <label class="form-control-label" for="email">Email</label>
-            <input class="form-control" id="email" type="text" name="email">
+            <input class=${'form-control' + (errorMsg ? ' is-invalid' : '')} id="email" type="text" name="email">
         </div>
         <div class="form-group">
             <label class="form-control-label" for="password">Password</label>
-            <input class="form-control" id="password" type="password" name="password">
+            <input class=${'form-control' + (errorMsg ? ' is-invalid' : '')} id="password" type="password" name="password">
         </div>
         <input type="submit" class="btn btn-primary" value="Login" />
     </div>
@@ -33,14 +34,18 @@ export function showLogin(cxt) {
         e.preventDefault();
         let form = e.currentTarget
         let formData = new FormData(form);
-    
-        let email = formData.get('email');
-        let password = formData.get('password');
-    
-        const user = await loginRequest({ email, password });
-        localStorage.setItem('user', JSON.stringify(user))
-        form.reset();
-        cxt.updateNav();
-        cxt.page.redirect('/');
+
+        let email = formData.get('email').trim();
+        let password = formData.get('password').trim();
+
+        try {
+            const user = await loginRequest({ email, password });
+            localStorage.setItem('user', JSON.stringify(user))
+            form.reset();
+            cxt.updateNav();
+            cxt.page.redirect('/');
+        } catch (err) {
+            render(loginTemplate(login, err.message), container);
+        }
     }
 }
