fe6f64b99129b8e2dae2d821bd4447eb8f57c8c4
Todor Mladenov <toshko958@gmail.com>
2023-11-15 12:56:40 +0100

    Update details.js

--- JS-Applications-Exercises/06.Routing/src/details.js
+++ JS-Applications-Exercises/06.Routing/src/details.js
@@ -1,9 +1,8 @@
 import { render, html } from "../node_modules/lit-html/lit-html.js";
 import { createDeleteRequest, getFurnitureById } from "./requests.js";
 const container = document.querySelector('.container');
-const user = JSON.parse(localStorage.getItem('user'));
 
-const detailsTemplate = (itemData, deletePost) => html`
+const detailsTemplate = (itemData, isOwner, deletePost) => html`
 <div class="row space-top">
             <div class="col-md-12">
                 <h1>Furniture Details</h1>
@@ -24,22 +23,23 @@ const detailsTemplate = (itemData, deletePost) => html`
                 <p>Description: <span>${itemData.description}</span></p>
                 <p>Price: <span>${itemData.price}</span></p>
                 <p>Material: <span>${itemData.material}</span></p>
-                ${user && user._id == itemData._ownerId ? 
+                ${isOwner ?
                     html`
                     <div>
                         <a href="/edit/${itemData._id}" class="btn btn-info">Edit</a>
-                        <a href="/delete" class="btn btn-red" @click=${deletePost}>Delete</a>
-                    </div>` :
-                    html``}
+                        <a href="javascript:void(0)" class="btn btn-red" @click=${deletePost}>Delete</a>
+                    </div>` : null}
             </div>
 </div>`;
 
-export async function showDetails(ctx){
+export async function showDetails(ctx) {
     let itemData = await getFurnitureById(ctx.params.id)
-    render(detailsTemplate(itemData, deletePost), container);
-
-    async function deletePost(e){
-        e.preventDefault();
+    const user = JSON.parse(localStorage.getItem('user'));
+    const isOwner = user && user._id == itemData._ownerId;
+    
+    render(detailsTemplate(itemData, isOwner, deletePost), container);
+    
+    async function deletePost() {
         await createDeleteRequest(ctx.params.id);
         ctx.page.redirect('/');
     }
