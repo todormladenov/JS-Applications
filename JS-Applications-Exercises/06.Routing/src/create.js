25636dfb501bcc258fc243a11bf70e3d0b33ea19
Todor Mladenov <toshko958@gmail.com>
2023-11-15 12:57:02 +0100

    Update error handling

--- JS-Applications-Exercises/06.Routing/src/create.js
+++ JS-Applications-Exercises/06.Routing/src/create.js
@@ -3,7 +3,7 @@ import { createRequest } from "./requests.js";
 
 const container = document.querySelector('.container');
 
-const createTemplate = (createFurniture) => html`
+const createTemplate = (createFurniture, errorMsg, errors) => html`
 <div class="row space-top">
             <div class="col-md-12">
                 <h1>Create New Furniture</h1>
@@ -11,33 +11,34 @@ const createTemplate = (createFurniture) => html`
             </div>
         </div>
         <form @submit=${createFurniture}>
+        ${errorMsg ? html`<div class="form-group">${errorMsg}</div>` : null}
             <div class="row space-top">
                 <div class="col-md-4">
                     <div class="form-group">
                         <label class="form-control-label" for="new-make">Make</label>
-                        <input class="form-control valid" id="new-make" type="text" name="make">
+                        <input class=${'form-control' + (errors.make ? ' is-invalid' : '')} id="new-make" type="text" name="make">
                     </div>
                     <div class="form-group has-success">
                         <label class="form-control-label" for="new-model">Model</label>
-                        <input class="form-control is-valid" id="new-model" type="text" name="model">
+                        <input class=${'form-control' + (errors.model ? ' is-invalid' : '')} id="new-model" type="text" name="model">
                     </div>
                     <div class="form-group has-danger">
                         <label class="form-control-label" for="new-year">Year</label>
-                        <input class="form-control is-invalid" id="new-year" type="number" name="year">
+                        <input class=${'form-control' + (errors.year ? ' is-invalid' : '')} id="new-year" type="number" name="year">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-description">Description</label>
-                        <input class="form-control" id="new-description" type="text" name="description">
+                        <input class=${'form-control' + (errors.description ? ' is-invalid' : '')} id="new-description" type="text" name="description">
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="form-group">
                         <label class="form-control-label" for="new-price">Price</label>
-                        <input class="form-control" id="new-price" type="number" name="price">
+                        <input class=${'form-control' + (errors.price ? ' is-invalid' : '')} id="new-price" type="number" name="price">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-image">Image</label>
-                        <input class="form-control" id="new-image" type="text" name="img">
+                        <input class=${'form-control' + (errors.img ? ' is-invalid' : '')} id="new-image" type="text" name="img">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-material">Material (optional)</label>
@@ -50,23 +51,33 @@ const createTemplate = (createFurniture) => html`
 
 export function showCreate(cxt) {
     cxt.updateNav();
-    render(createTemplate(createFurniture), container);
+    render(createTemplate(createFurniture, null, {}), container);
 
     async function createFurniture(e) {
         e.preventDefault();
         let form = e.currentTarget
-        let formData = new FormData(form);
+        let formData = [...(new FormData(form)).entries()];
 
-        let make = formData.get('make');
-        let model = formData.get('model');
-        let year = formData.get('year');
-        let description = formData.get('description');
-        let price = formData.get('price');
-        let img = formData.get('img');
-        let material = formData.get('material');
+        const data = formData.reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
+        const missing = formData.filter(([k, v]) => k != 'material' && v.trim() == '');
 
-        await createRequest({ make, model, year, description, price, img, material });
-        form.reset();
-        cxt.page.redirect('/');
+        try {
+            if (missing.length) {
+                const errors = missing.reduce((a, [k]) => Object.assign(a, { [k]: true }), {})
+                throw {
+                    error: new Error('Please fill all mandatory fields!'),
+                    errors
+                };
+            }
+            data.year = Number(data.year);
+            data.price = Number(data.price);
+
+            await createRequest(data);
+            form.reset();
+            cxt.page.redirect('/');
+        } catch (err) {
+            const message = err.message || err.error.message;
+            render(createTemplate(createFurniture, message, err.errors), container);
+        }
     }
 }
