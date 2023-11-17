69abfcbdefd153be8d78a60a4818b9998a315e13
Todor Mladenov <toshko958@gmail.com>
2023-11-17 20:51:02 +0100

    Created login functionality

---
+++ Exam Prep/03.Fruitipedia_Resources/src/login.js
@@ -0,0 +1,38 @@
+import { html } from "../node_modules/lit-html/lit-html.js";
+import { loginRequest } from "./requests.js";
+
+const loginTemplate = (onSubmit) => html`
+<section id="login">
+<div class="form">
+  <h2>Login</h2>
+  <form class="login-form" @submit=${onSubmit}>
+    <input type="text" name="email" id="email" placeholder="email" />
+    <input type="password" name="password" id="password" placeholder="password" />
+    <button type="submit">login</button>
+    <p class="message">
+      Not registered? <a href="#">Create an account</a>
+    </p>
+  </form>
+</div>
+</section>`;
+
+export function viewLogin(ctx) {
+  ctx.render(loginTemplate(onSubmit));
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const email = formData.get('email').trim();
+    const password = formData.get('password').trim();
+
+    if (email == '' || password == '') {
+      return alert('All fields are required!');
+    }
+
+    const user = await loginRequest({ email, password });
+    localStorage.setItem('user', JSON.stringify(user));
+    ctx.page.redirect('/');
+  }
+}
